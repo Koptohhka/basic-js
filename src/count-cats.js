e49a197f11277df9027dd627e249f59ef3a872ec
Pan Artur <artur.pankov.16@list.ru>
2020-02-25 19:11:40 +0300

    feat: add test resolution dream-team task

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,12 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  let count = 0;
+  matrix.forEach((it) => {
+    for (let i = 0; i < it.length; i++) {
+      if (it[i] === '^^') {
+        count += 1;
+      }
+    }
+  });
+
+  return count;
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,46 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let arr = [];
+  let res = '';
+
+  let testArr = [];
+
+  if (Array.isArray(members)) {
+    
+  } else {
+    return false;
+  }
+
+  for (let j = 0; j < members.length; j++) {
+    if (typeof members[j] === 'string') {
+      arr.push(members[j]);
+    }
+  }
+
+  arr.forEach((it) => {
+    for (let i = 0; i < it.length; i++) {
+      if (it[i] !== ' ') {
+        testArr.push(it[i].toUpperCase());
+        break;
+      }
+    }
+
+  })  
+
+  for (let i = 0; i < testArr.length; i++) {
+      
+    for (let j = 0; j < testArr.length; j++) {
+      if (testArr[i] < testArr[j]) {
+        let swap = testArr[i];
+        testArr[i] = testArr[j];
+        testArr[j] = swap;
+      }
+    }
+
+  }
+
+  for (let i = 0; i < testArr.length; i++) {
+    res += testArr[i];
+  }
+
+  return res;
 };
